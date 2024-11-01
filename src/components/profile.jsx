6580288d9bd00f0dfd75c1e8c169d425a1a1e496
SlviACH6 <146087759+SlviACH6@users.noreply.github.com>
2024-11-01 23:54:17 +0100

    Update profile.jsx

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function profile() {
   return (
     <div>
@@ -7,8 +9,15 @@ export default function profile() {
       <form>
         <h2>Name</h2>
         <input type="text" placeholder="choose a name for your avatar" />
-        <h2></h2>
-        <input type="text" />
+        <h2>student A</h2>
+        <input type="radio" />
+        <h2>student B</h2>
+        <input type="radio" />
+        <Link to="/map">
+          <button>
+            <strong> let s go!</strong>link to ubs game
+          </button>
+        </Link>
       </form>
     </div>
   );
