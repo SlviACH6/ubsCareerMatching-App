208ce40693b5822cda63b3a65c71e546d68fd522
SlviACH6 <146087759+SlviACH6@users.noreply.github.com>
2024-11-01 23:43:03 +0100

    Update profile.jsx

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,7 +1,15 @@
 export default function profile() {
   return (
     <div>
-      <h1>pofile and avatar</h1>
+      <h1>choose your avatar</h1>
+      <img src="https://randomuser.me/api/portraits/lego/6.jpg" alt="avatar1" />
+      <img src="https://randomuser.me/api/portraits/lego/1.jpg" alt="avatar2" />
+      <form>
+        <h2>Name</h2>
+        <input type="text" placeholder="choose a name for your avatar" />
+        <h2></h2>
+        <input type="text" />
+      </form>
     </div>
   );
 }
