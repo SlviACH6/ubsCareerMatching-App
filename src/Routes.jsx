a1fbd1275251667ed78f70cc4eae5b5145575823
SlviACH6 <146087759+SlviACH6@users.noreply.github.com>
2024-11-01 20:40:52 +0100

    fixing routes

---
+++ src/Routes.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Banking from "./components/banking";
+import Bem from "./components/bem";
+import It from "./components/it";
+import ItWayUp from "./components/itWayUp";
+import Main from "./components/main";
+import Map from "./components/map";
+import Profile from "./components/main";
+import Results from "./components/results";
+
+// Routes
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/banking" element={<Banking />} />
+        <Route path="/bem" element={<Bem />} />
+        <Route path="/it" element={<It />} />
+        <Route path="/itWayUp" element={<ItWayUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/results" element={<Results />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
