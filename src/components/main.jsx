f9ebb2245917e124c4c99ca8d5f08ca0f2ed813a
SlviACH6 <146087759+SlviACH6@users.noreply.github.com>
2024-11-01 22:54:36 +0100

    Update main.jsx
    updates on landing page

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,7 +1,10 @@
-export default function Hello() {
+export default function landingPage() {
   return (
     <div>
-      <h1>LANDING PAGE FOR OUR UBS APP</h1>
+      <h1>title of the challenge</h1>
+      <h1>slogan</h1>
+      <h2>description and porpusse</h2>
+      <button><strong>Let´s go!</strong> link to profile</button>
     </div>
   );
 }
