74c4b7ab3ac012686fa23a11c12007d36a6d9236
SlviACH6 <146087759+SlviACH6@users.noreply.github.com>
2024-11-02 13:41:22 +0100

    updates

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,6 @@
 // Router.js
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Banking from "./components/banking";
 import Bem from "./components/bem";
 import It from "./components/it";
@@ -12,20 +12,18 @@ import Results from "./components/results";
 
 function Router() {
   return (
-    <div className="max-w-[390px] max-h-[845px] mx-auto overflow-y-auto border border-gray-300">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/banking" element={<Banking />} />
-          <Route path="/bem" element={<Bem />} />
-          <Route path="/it" element={<It />} />
-          <Route path="/itWayUp" element={<ItWayUp />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/map" element={<Map />} />
-          <Route path="/results" element={<Results />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/banking" element={<Banking />} />
+        <Route path="/bem" element={<Bem />} />
+        <Route path="/it" element={<It />} />
+        <Route path="/itWayUp" element={<ItWayUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/results" element={<Results />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,16 +1,25 @@
 import { Link } from "react-router-dom";
+import UBS_Logo from "../assets/UBS_Logo.png";
 
-export default function landingPage() {
+export default function main() {
   return (
-    <div>
-      <h1>USB Career Pathfinder</h1>
-      <h1>Explore, discover and choose your UBS carrer path</h1>
-      <h2>description and porpusse</h2>
-      <Link to="/profile">
-        <button>
-          <strong> let s go!</strong>link to profile
-        </button>
-      </Link>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 font-light">
+      <div className="w-[390px] h-[645px] text-center p-4 bg-white rounded-lg shadow-lg">
+        <div className="flex flex-col justify-center items-center m-6">
+          <img className="w-40" src={UBS_Logo} alt="logo" />
+          <h1 className="text-lm mt-6 font-bold mb-2">
+            Explore, discover and choose your UBS career path
+          </h1>
+        </div>
+        <div>
+          <video src="" className="w-full rounded" controls></video>
+        </div>
+        <Link to="/profile">
+          <button className="bg-red-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-12">
+            <strong> Let´s Start!</strong>
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
